e6c4eb26b9b713df1a74ec06042f6f99e1ad2702
dontlove <a1150643534@sina.com>
2019-12-17 14:46:23 +0800

    0.0.7

--- person/lishuai/LockDemo/app/src/main/java/com/example/a1150643534/myapplication/MainActivity.java
+++ person/lishuai/LockDemo/app/src/main/java/com/example/a1150643534/myapplication/MainActivity.java
@@ -10,8 +10,9 @@ public class MainActivity extends AppCompatActivity {
     private Button button2;
     private Button button3;
     private Button button4;
-    private int level;
-    private int maxLevel;
+    private int level = 2;
+    private int maxLevel = 4;
+    private int minLevel = 1;
 
 
     @Override
@@ -23,25 +24,37 @@ public class MainActivity extends AppCompatActivity {
         button3 = findViewById(R.id.button3);
         button4 = findViewById(R.id.button4);
         button1.setClickable(false);
+        button1.setBackgroundResource(R.drawable.lock);
         button2.setClickable(false);
+        button2.setBackgroundResource(R.drawable.lock);
         button3.setClickable(false);
+        button3.setBackgroundResource(R.drawable.lock);
         button4.setClickable(false);
-        level = 1;
-        maxLevel = 4;
+        button4.setBackgroundResource(R.drawable.lock);
         if(level==1){
             button1.setClickable(true);
+            button1.setBackgroundResource(R.drawable.empty);
         }else if(level==2){
             button1.setClickable(true);
+            button1.setBackgroundResource(R.drawable.empty);
             button2.setClickable(true);
+            button2.setBackgroundResource(R.drawable.empty);
         }else if(level==3){
             button1.setClickable(true);
+            button1.setBackgroundResource(R.drawable.empty);
             button2.setClickable(true);
+            button2.setBackgroundResource(R.drawable.empty);
             button3.setClickable(true);
+            button3.setBackgroundResource(R.drawable.empty);
         }else if(level==4){
             button1.setClickable(true);
+            button1.setBackgroundResource(R.drawable.empty);
             button2.setClickable(true);
+            button2.setBackgroundResource(R.drawable.empty);
             button3.setClickable(true);
+            button3.setBackgroundResource(R.drawable.empty);
             button4.setClickable(true);
+            button4.setBackgroundResource(R.drawable.empty);
 
         }
 
